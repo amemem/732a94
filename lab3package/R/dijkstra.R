4ea44139a9e2d9865ae6c96d669ad7a96ec9fc5f
amemem <ameme698@student.liu.se>
2021-09-13 20:02:48 +0200

    Update variable name

--- lab3package/R/dijkstra.R
+++ lab3package/R/dijkstra.R
@@ -3,7 +3,7 @@
 #' @description Computes the shortest paths from an initial node to all other nodes in a graph.
 #' 
 #' @param graph A data frame with 3 variables labeled \emph{v1}, \emph{v2} and \emph{w}.
-#' @param init The starting node where all paths are to be computed from.
+#' @param init_node The starting node where all paths are to be computed from.
 #' 
 #' @return A vector with the distances from the initial node to all nodes.
 #' 
@@ -11,17 +11,17 @@
 #' 
 #' @source \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
 #' 
-dijkstra = function(graph, init) {
+dijkstra = function(graph, init_node) {
   stopifnot(is.data.frame(graph),
             ncol(graph) == 3,
             names(graph) == c("v1", "v2", "w"),
             sapply(graph, is.numeric),
-            is.numeric(init),
-            any(graph[1:2] == init))
+            is.numeric(init_node),
+            any(graph[1:2] == init_node))
   unseen = as.vector(unique(unlist(graph[["v1"]])))
   path = rep(Inf, length(unseen))
-  path[match(init, unseen)] = 0
-  root = init
+  path[match(init_node, unseen)] = 0
+  root = init_node
   repeat {
     nodes = subset(graph, v1 == root & v2 %in% intersect(v2, unseen))
     nrnodes = length(nodes[["v2"]])
